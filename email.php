326acef511ba53f777a90725ff06bb4e29c99f70
hlaing tin htun <hlaingtinhtun@gmail.com>
2020-04-21 20:22:51 +0900

    Update email.php

--- email.php
+++ email.php
@@ -3,12 +3,3 @@
 echo "sending email";
 echo "another email sent";
 
-echo 'thrid line from cli';
-echo 'third line from online';
-
-echo 'forth line from cli';
-echo 'forth line from online';
-
-echo "fifth line from online";
-  
-echo 'fifth line from cli';  
